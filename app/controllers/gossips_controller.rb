2e792c12e64849c4e04718dffbdfbe3766094885
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-31 18:26:44 +0100

    Le formulaire marche BORDEL et show aussi

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,23 @@
+class GossipsController < ApplicationController
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+  	@gossip = Gossip.new(gossip_params)
+    @gossip.save
+    redirect_to @gossip
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def gossip_params
+    params.require(:gossip).permit(:anonymous_author, :content)
+  end
+
+
+
+end
